75957755d1d771ebc19526b8ba8b7c35660ef167
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2019-11-12 16:01:19 +0900

    アドレス設置

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,16 @@ Rails.application.routes.draw do
       get '/elevator_pitch/:product_id', to: 'elevator_pitch#show'
       post '/elevator_pitch/:product_id', to:'elevator_pitch#create'
       patch '/elevator_pitch/:product_id', to:'elevator_pitch#update'
+
+      get '/user_story_map', to:'user_story_map#index'
+      get '/user_story_map/:product_id', to: 'user_story_map#show'
+      post '/user_story_map/:product_id', to:'user_story_map#create'
+      patch '/user_story_map/:product_id', to:'user_story_map#update'
+
+      get '/product_backlog', to:'product_backlog#index'
+      get '/product_backlog/:product_id', to: 'product_backlog#show'
+      post '/product_backlog/:product_id', to:'product_backlog#create'
+      patch '/product_backlog/:product_id', to:'product_backlog#update'
     end
   end
 end
