29ce291f519e9927ef42031f5b26ec5cf672e66d
Uehara Yuuku <e175732@ie.u-ryukyu.ac.jp>
2019-11-12 15:19:29 +0900

    エレベーターピッチ用のコントローラーを追加

---
+++ app/controllers/api/v1/elevator_pitch_controller.rb
@@ -0,0 +1,65 @@
+module Api
+  module V1
+    class ElevatorPitchController < ApplicationController
+      # 一覧を取得
+      def index
+        render json: { status: 'SUCCESS', elevator_pitchies: ElevatorPitch.all }
+      end
+
+      def show
+        elevator_pitch = ElevatorPitch.find_by(product_id: params[:product_id])
+        if elevator_pitch.present?
+          render json: { status: 'SUCCESS', product_id: elevator_pitch.product_id , board_texts: elevator_pitch.board_texts }
+        else
+          render json: { status: 'FAILED' }
+        end
+      end
+
+      def create
+        if ElevatorPitch.find_by(product_id: params[:product_id]).blank?
+          elevator_pitch = ElevatorPitch.new()
+          elevator_pitch.product_id = params[:product_id]
+          elevator_pitch.board_texts = {
+              txt1: { text: '' },
+              txt2: { text: '' },
+              txt3: { text: '' },
+              txt4: { text: '' },
+              txt5: { text: '' },
+              txt6: { text: '' },
+              txt7: { text: '' },
+          }
+          if elevator_pitch.save
+            render json: { status: 'SUCCESS', product_id: elevator_pitch.product_id }
+          else
+            render json: { status: 'FAILED' }
+          end
+        else
+          render json: { status: 'FAILED' }
+        end
+      end
+
+      def update
+        elevator_pitch = ElevatorPitch.find_by(product_id: params[:product_id])
+        if elevator_pitch.update_attributes(board_texts: text_params)
+          render json: { status: 'SUCCESS', product_id: elevator_pitch.product_id , board_texts: elevator_pitch.board_texts }
+        else
+          render json: { status: 'FAILED' }
+        end
+      end
+
+      # パラメータを取得する
+      private
+        def text_params
+          params.require(:board_texts).permit(
+              txt1: [:text ],
+              txt2: [:text ],
+              txt3: [:text ],
+              txt4: [:text ],
+              txt5: [:text ],
+              txt6: [:text ],
+              txt7: [:text ],
+          )
+        end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,23 @@ Rails.application.routes.draw do
     namespace 'v1' do
       get '/hello', to: 'hello#hello'
       get '/board', to: 'board#index'
+      
       get '/boardcards', to:'board#card_data'
       post '/boardcards', to:'board#new'
       delete '/boardcards', to:'board#delete_data'
+
       get '/products', to:'products#index'
       post '/products', to:'products#create'
+
       get '/leancanvas', to:'leancanvas#index'
       get '/leancanvas/:product_id', to: 'leancanvas#show'
       post '/leancanvas/:product_id', to:'leancanvas#create'
       patch '/leancanvas/:product_id', to:'leancanvas#update'
+
+      get '/elevator_pitch', to:'elevator_pitch#index'
+      get '/elevator_pitch/:product_id', to: 'elevator_pitch#show'
+      post '/elevator_pitch/:product_id', to:'elevator_pitch#create'
+      patch '/elevator_pitch/:product_id', to:'elevator_pitch#update'
     end
   end
 end
